72b7432234e66c63f07149e8e23e17eda6e327bc
whysln <wylwang@gmail.com>
2016-10-24 23:30:36 +0800

    update

--- 09_juggling_async.js
+++ 09_juggling_async.js
@@ -1,21 +1,22 @@
 var http = require("http");
 
 var contents = [];
-var status = 0;
+var finishedFlag = 0;
 
-function getContents(i, callback) {
-	contents.push(i);
-	http.get(process.argv[i + 2], function(response){
-		var str = '';
-		response.on("data", function(data){
-			str += data.toString();
-		});
-		response.on("end", function(){
-			contents.push(str);
-			status += 1;
-		});
-	});
+for (var i = 0; i < 3; i++) {
+	if (finishedFlag = 2) {
+		console.log(contents[0]);
+		console.log(contents[1]);
+		console.log(contents[2]);
 	} else {
-
+		contents[i] = '';
+		http.get(process.argv[i + 2], function(response){
+			response.on("data", function(data){
+				contents[i] += data.toString();
+			});
+			response.on("end", function(){
+				finishedFlag += 1;
+			});
+		});
 	}
 }
